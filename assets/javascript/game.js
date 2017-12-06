5eb5165c62606c0819bc84e54c0c305164f9403e
unknown <jkue3000@gmail.com>
2017-12-06 17:49:45 -0500

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,78 @@
-var facebook = Math.random(Math.floor() * 12) +1;
-var instagram = Math.random(Math.floor() * 12) +1;
-var snapchat = Math.random(Math.floor() * 12) +1;
-var twitter = Math.random(Math.floor() * 12) +1;
+// set the buttons to random #'s'
+var facebook = Math.floor(Math.random() * 12) +1;
+var instagram = Math.floor(Math.random() * 12) +1;
+var snapchat = Math.floor(Math.random() * 12) +1;
+var twitter = Math.floor(Math.random() * 12) +1;
 
-randomNumGen = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-console.log(facebook);
-// button listeners for clicks on the buttons
+// generate computer number
+var randomNumGen = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
+// blank vars for wins/losses and userClickValue
+var userValue= "";
 
+wins = 0;
+loss = 0;
 
-// the random number generator for the number to guess 19 - 120
+// show RandomNum to HTML
+$('.randomNumGen').html(randomNumGen);
 
-// random number generator for each button
+// button listeners for clicks on the buttons and fucntions
+$('.facebook').on('click', function(){
+	if (facebook > 0){
+		userValue = parseInt(userValue + facebook);
+		$('.buttons-score').html(userValue);
+		gameCheck();
+		console.log(facebook);
+	};
+});
 
-// game reset if value is over or equal
+$('.snapchat').on('click', function(){
+	if (snapchat > 0){
+		userValue = parseInt(userValue + snapchat);
+		$('.buttons-score').html(userValue);
+		gameCheck();
+	};
+});
 
-// button clicker adder that adds the total to the button click total
+$('.instagram').on('click', function(){
+	if (instagram > 0){
+		userValue = parseInt(userValue + instagram);
+		$('.buttons-score').html(userValue);
+		gameCheck();
+	};
+});
 
-// ADD ALL TO THE HTML and a HTML UPDATER
+$('.twitter').on('click', function(){
+	if (twitter > 0){
+		userValue = parseInt(userValue + twitter);
+		$('.buttons-score').html(userValue);
+		gameCheck();
+	};
+});
+
+// function for win or lose
+function gameCheck(){
+	if (randomNumGen == userValue){
+		wins++;
+		$('.winLossAlert').html("You Won!");
+		$('#win').html("Wins:" + " " + wins);
+		reset();
+	} else if (randomNumGen < userValue){
+		loss++;
+		$('.winLossAlert').html("You Lost! Try Again!");
+		$('#loss').html("Losses:" + " " + loss);
+		reset();
+	};
+};
+
+// Function to reset game if win or lose
+function reset(){
+	facebook = Math.floor(Math.random() * 12) +1;
+	instagram = Math.floor(Math.random() * 12) +1;
+	snapchat = Math.floor(Math.random() * 12) +1;
+	twitter = Math.floor(Math.random() * 12) +1;
+	randomNumGen = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+	$('.randomNumGen').html(randomNumGen);
+	userValue = "";
+	$('.buttons-score').html("<h2>Match your # to the computer # below</h2>");
+};
