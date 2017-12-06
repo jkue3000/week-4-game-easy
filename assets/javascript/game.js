a92cbe2106ad1f3cf1ca1890860a1465e1bc8781
unknown <jkue3000@gmail.com>
2017-12-06 02:00:39 -0500

    updated HTML/CSS/JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var facebook = Math.random(Math.floor() * 12) +1;
+var instagram = Math.random(Math.floor() * 12) +1;
+var snapchat = Math.random(Math.floor() * 12) +1;
+var twitter = Math.random(Math.floor() * 12) +1;
+
+randomNumGen = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+console.log(facebook);
+// button listeners for clicks on the buttons
+
+
+
+// the random number generator for the number to guess 19 - 120
+
+// random number generator for each button
+
+// game reset if value is over or equal
+
+// button clicker adder that adds the total to the button click total
+
+// ADD ALL TO THE HTML and a HTML UPDATER
